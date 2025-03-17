65dc2cb835ef1e6d8a3aa5bd59714e426641d770
Fuan200 <juandiazfdez1992@gmail.com>
2025-03-17 13:46:12 -0600

    translate project & do botom to change language

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://fuan200.github.io/'
+    site: 'https://fuan200.github.io/',
+    i18n: {
+        defaultLocale: 'es',
+        locales: [ 'es', 'en' ],
+        routing: {
+            prefixDefaultLocale: false,
+        }
+    }
 });
