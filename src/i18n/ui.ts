071036bcb21c092e71a689ac71214a1b46df320c
Fuan200 <juandiazfdez1992@gmail.com>
2025-04-22 15:01:18 -0600

    modify i18n button & create navbar

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+    es: 'Español',
+    en: 'English',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+    es: {
+        'nav.home': 'Inicio',
+        'nav.blog': 'Blog',
+        'nav.setup': 'Setup',
+    },
+    en: {
+        'nav.home': 'Home',
+        'nav.blog': 'Blog',
+        'nav.setup': 'Setup',
+    },
+} as const;
