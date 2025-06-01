10a877a1459e7f59b57b5578b82fc343c32add45
Fuan200 <juandiazfdez1992@gmail.com>
2025-05-31 20:35:37 -0600

    move es to .. and now / is spanish lang /en this does not exist now, fix cv on jsons

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,7 @@ export default defineConfig({
         defaultLocale: 'es',
         locales: [ 'es', 'en' ],
         routing: {
-            prefixDefaultLocale: true,
+            prefixDefaultLocale: false,
         }
     },
-    middleware: true,
-    output: 'server',
 });
